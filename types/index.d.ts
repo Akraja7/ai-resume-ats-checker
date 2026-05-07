acf4a3bc624bd3ba0301c63ac9fe76dcab024733
安烨 <139227468+Akraja7@users.noreply.github.com>
2026-05-08 00:34:08 +0800

    Add files via upload

---
+++ types/index.d.ts
@@ -0,0 +1,63 @@
+interface Job {
+  title: string;
+  description: string;
+  location: string;
+  requiredSkills: string[];
+}
+
+interface Resume {
+  id: string;
+  companyName?: string;
+  jobTitle?: string;
+  imagePath: string;
+  resumePath: string;
+  feedback: Feedback;
+}
+
+interface Feedback {
+  overallScore: number;
+  ATS: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+    }[];
+  };
+  toneAndStyle: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  content: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  structure: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+  skills: {
+    score: number;
+    tips: {
+      type: "good" | "improve";
+      tip: string;
+      explanation: string;
+    }[];
+  };
+}
+
+declare module "pdfjs-dist/build/pdf.mjs";
+declare module "pdfjs-dist/legacy/build/pdf.mjs";
+declare module "pdfjs-dist/build/pdf.worker.min.mjs?url";
+declare module "pdfjs-dist/legacy/build/pdf.worker.min.mjs?url";
